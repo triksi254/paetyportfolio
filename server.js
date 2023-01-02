59d9d04297d77e00ab9a9340f8ab4279f93621b2
triksi254 <89916911+triksi254@users.noreply.github.com>
2023-01-02 18:12:07 +0300

    make it better

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ if (process.env.NODE_ENV === "production") {
     );
 }
 
-const PORT = process.env.port || "5000";
+const port = process.env.PORT || 5000;
 app.listen(port, console.log(`server listing to port 5000 only`));
